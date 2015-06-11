ecde109f68a09497fb5615df9c7a70a9869dd3c7
Oleg <o.volodin@yahoo.com>
2015-06-11 09:51:31 +0300

    create scraper

--- scraper.py
+++ scraper.py
@@ -8,32 +8,30 @@ import re
 user_agent = {'User-Agent': 'Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)'}
 
 
-with open('test.txt') as f:
-
-        todays_date = ''
+with open('test.txt') as f:    # open test.txt file containing the list of url
         url_empty = set()
-        for url in f.readlines():
+        for url in f.readlines():      # read every line of the file
             try:
-                pages = requests.get(url.strip(), headers = user_agent)
+                pages = requests.get(url.strip(), headers = user_agent)    # make a request to the url
             except: continue
             soup = bs(pages.text)
             title =  soup.find('title').text.encode('utf-8')
             print title
-            tag = soup.find(text = re.compile('There is a newer edition of this item'))
-            if tag:
-                newed_asin = tag.find_next('a')['href'].split('dp/')[1].split('/')[0]
-                asin = url.split('dp/')[-1]
+            tag = soup.find(text = re.compile('There is a newer edition of this item'))  # find the text on the page
+            todays_date = str(datetime.now())
+            if tag:                                                                      # if text found
+                newed_asin = tag.find_next('a')['href'].split('dp/')[1].split('/')[0]    # get asin of a new edition
+                asin = url.split('dp/')[-1]                                              # get asin of the previous edition
                 flag_asin = ''
-                todays_date = str(datetime.now())
-                if url in url_empty:
-                    flag_asin = url.split('dp/')[1].split('/')[0]
-                    url_empty.remove(url)
+                if url in url_empty:          # if the url is in the set of the urls with no text on the page
+                    flag_asin = newed_asin    # get asin and set the flag
+                    url_empty.remove(url)     # remove the url from the set
                 print asin
             else:
-                url_empty.add(url)
+                url_empty.add(url)            # if no text on the page, add the url to the set
                 asin = url.split('dp/')[-1]
                 newed_asin = ''
-                flag_asin = 'there is no a newer edition of this item'
+                flag_asin = 'there is no a newer edition of this item'   # set the flag that there is no a newer edition of this item
                 print flag_asin
-            scraperwiki.sqlite.save(unique_keys=['asin'], data={"asin": asin, "new_edition_asin": newed_asin, "flag": flag_asin, "d": todays_date })
+            scraperwiki.sqlite.save(unique_keys=['asin'], data={"asin": asin, "new_edition_asin": newed_asin, "flag": flag_asin, "d": todays_date })  # save data into sql database
 
